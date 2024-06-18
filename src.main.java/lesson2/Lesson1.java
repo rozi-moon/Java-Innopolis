d8a0907be4955fc183ae6ce33c5d48a7ca3e2578
rozaliagirfanova <girf.rozalia@gmail.com>
2024-06-18 16:37:23 +0300

    Задание 2

---
+++ src.main.java/lesson2/Lesson1.java
@@ -0,0 +1,12 @@
+package lesson2;
+import java.util.Scanner;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double f = sc.nextDouble();
+        double c = (f - 32) * 5/9;
+        System.out.println(c);
+
+    }
+}
---
+++ src.main.java/lesson2/Lesson2.java
@@ -0,0 +1,29 @@
+package lesson2;
+import java.util.Scanner;
+public class Lesson2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        int minus = a - b;
+        int mult = a * b;
+        int mean = (a + b) / 2;
+        int distant = a - b;
+        int max = Math.max(a, b);
+        int min = Math.max(a, b);
+                System.out.println(
+                        "Сумма двух целых чисел: " + sum +
+                        "\nРазница двух целых чисел: " + minus +
+                        "\nПроизведение из двух целых чисел: " + mult +
+                        "\nСреднее из двух целых чисел: " + mean +
+                        "\nРасстояние двух целых чисел: " + distant +
+                        "\nМаксимальное целое число: " + max +
+                        "\nМинимальное целое число: " + min );
+
+
+
+
+
+    }
+}
