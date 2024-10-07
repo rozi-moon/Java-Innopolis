6302a5144dfeba84d7b53232fcf7641d1ba5beb6
rozaliagirfanova <girf.rozalia@gmail.com>
2024-10-07 19:50:53 +0300

    Homework 08

---
+++ src.main.java/homework08/Product.java
@@ -0,0 +1,58 @@
+package homework08;
+
+import java.util.Objects;
+
+public class Product {
+
+    private String name;
+
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            System.out.println("Имя не должно быть пустой строкой");
+        } else {
+            this.name = name;
+        }
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        if (price < 0) {
+            System.out.println("Цена не может быть отрицательным числом");
+            this.price = 0;
+        } else {
+            this.price = price;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (!(o instanceof Product product)) return false;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+}
