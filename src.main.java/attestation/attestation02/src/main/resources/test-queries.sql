b3cf3c0d1d14b56692de13008632159821a134e9
rozaliagirfanova <girf.rozalia@gmail.com>
2024-10-21 10:21:52 +0300

    Attestation 2 1

--- src.main.java/attestation/attestation02/src/main/resources/test-queries.sql
+++ src.main.java/attestation/attestation02/src/main/resources/test-queries.sql
@@ -4,6 +4,6 @@ select * from product where count > 20;
 update person set name = 'Андрей' where id = 3
 update product set count = 1000, price = 250 where description = 'Кофе';
 update "order" set date = now() where person_id = 3;
-delete from "order";
+delete from "order" where "order".product_id = 10;
 delete from person where id = 10;
 delete from product where price = 45;
